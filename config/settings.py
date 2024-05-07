cc93adc539c032443a0230b06a984174cec056b1
finka759 <finka759@gmail.com>
2024-05-07 21:35:19 +0300

    задание 1 готово

--- config/settings.py
+++ config/settings.py
@@ -54,8 +54,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'shop_20_1',
+        'USER': 'postgres',
+        'PASSWORD': '1616',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
